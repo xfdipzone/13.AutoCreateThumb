1a17d1a784580d8bfcd1cd55079c86e7a88780fa
xfdipzone <xfdipzone@gmail.com>
2014-01-05 00:44:28 +0800

    function update

--- supload/createthumb.php
+++ supload/createthumb.php
@@ -35,10 +35,44 @@ $source = str_replace('/'.$type.'/', '/'.$config['fromdir'].'/', $source_path.$r
 // 目标文件 
 $dest = $dest_path.$relative_url;
 
+if(!file_exists($source)){ // 原图不存在
+    exit();
+}
+
+// 高并发处理
+$processing_flag = '/tmp/thumb_'.md5($dest); // 用于判断文件是否处理中
+$is_wait = 0;                                // 是否需要等待
+$wait_timeout = 5;                           // 等待超时时间
+
+if(!file_exists($processing_flag)){
+    file_put_contents($processing_flag, 1, true);
+}else{
+    $is_wait = 1;
+}
+
+if($is_wait){ // 需要等待生成
+    while(file_exists($processing_flag)){
+        if(time()-$starttime>$wait_timeout){ // 超时
+            exit();
+        }
+        usleep(300000); // sleep 300 ms
+    }
+
+    if(file_exists($dest)){ // 图片生成成功
+        ob_clean();
+        header('content-type:'.mime_content_type($dest));
+        exit(file_get_contents($dest));
+    }
+}
+
 // 创建缩略图
 $obj = new PicThumb($logfile);
 $obj->set_config($config);
-if($obj->create_thumb($source, $dest)){
+$create_flag = $obj->create_thumb($source, $dest);
+
+unlink($processing_flag); // 删除处理中标记文件
+
+if($create_flag){ // 判断是否生成成功
     ob_clean();
     header('content-type:'.mime_content_type($dest));
     exit(file_get_contents($dest));
